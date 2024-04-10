54662cba7b496826a86c60f3194dd829f10975e3
ohitsnathalie <152981339+ohitsnathalie@users.noreply.github.com>
2024-04-10 19:38:15 +0200

    indentation testing

--- src/contexts/TaskContext.jsx
+++ src/contexts/TaskContext.jsx
@@ -3,28 +3,28 @@ import moment from "moment"
 
 const TaskListContext = createContext()
 const TaskListData = [
-	{
-		task: "Make a todolist app",
-		status: false,
-		created: moment().format("MMMM Do YYYY, h:mm:ss a"), //this is right now
-		deadline: moment().add(5, "days").calendar(), // this 24 h from now
-		topic: "Web development",
-		project: "Technigo",
-		importance: 1,
-		visible: true,
-	},
+  {
+    task: "Make a todolist app",
+    status: false,
+    created: moment().format("MMMM Do YYYY, h:mm:ss a"), //this is right now
+    deadline: moment().add(5, "days").calendar(), // this 24 h from now
+    topic: "Web development",
+    project: "Technigo",
+    importance: 1,
+    visible: true,
+  },
 ]
 
 export const TaskListProvider = ({ children }) => {
-	const [newTask, setNewTask] = useState([TaskListData])
+  const [newTask, setNewTask] = useState([TaskListData])
 
-	//remember to add ID!
+  //remember to add ID!
 
-	return (
-		<TaskListContext.Provider value={{ newTask, setNewTask }}>
-			{children}
-		</TaskListContext.Provider>
-	)
+  return (
+    <TaskListContext.Provider value={{ newTask, setNewTask }}>
+      {children}
+    </TaskListContext.Provider>
+  )
 }
 
 export const useTask = () => useContext(TaskListContext)
