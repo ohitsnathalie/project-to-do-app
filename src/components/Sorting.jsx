8e15575fbb6c71266ddd5e20f6a9b6736028dc61
Kathinka Sewell <4355167+kathinka@users.noreply.github.com>
2024-04-18 23:00:01 +0200

    Refactor Sorting component  to make sure the reset sort actually works correctly. added a variable to store the original list in, and made sure it got updated on new entries, but not when the list where sorted, so the original order was kept.

--- src/components/Sorting.jsx
+++ src/components/Sorting.jsx
@@ -1,8 +1,18 @@
-import { useTask } from "../contexts/TaskContext"
-import "./Sorting.css"
+import { useEffect, useState } from "react";
+import { useTask } from "../contexts/TaskContext";
+import "./Sorting.css";
 
 export const Sorting = () => {
   const { taskList, setTaskList } = useTask();
+  const [originalList, setOriginalList] = useState([]);
+
+
+  // make sure the original list is updated when the task list is updated, but only if the task list is longer than the original list so that the original list is not overwritten when sorting the tasks
+  useEffect(() => {
+    if (originalList.length === 0 || taskList.length > originalList.length){
+    setOriginalList([...taskList]);
+    }
+  },[taskList]);
 
   const sortTasks = (key) => {
     const sortedTasks = [...taskList].sort((a, b) => {
@@ -17,26 +27,21 @@ export const Sorting = () => {
     setTaskList(sortedTasks);
   };
 
-
-
-
-
   const resetSorting = () => {
-    setTaskList([...taskList]);
+    setTaskList([...originalList]);
   };
 
   return (
-
     <> <h2>🔍 Sort tasks</h2 >
-    <section className="sorting">
-      <button className="nav-btn" onClick={() => sortTasks('importance')}>Importance</button>
-      <button className="nav-btn" onClick={() => sortTasks('deadline')}>Deadline</button>
-      <button className="nav-btn" onClick={() => sortTasks('created')}>Created</button>
-      <button className="nav-btn" onClick={() => sortTasks('status')}>Status</button>
-      <button className="nav-btn" onClick={() => sortTasks('topic')}>Topic</button>
-      <button className="nav-btn" onClick={() => sortTasks('project')}>Project</button>
-      <button className="nav-btn" onClick={resetSorting}>Reset </button>
-    </section>
+      <section className="sorting">
+        <button className="nav-btn" onClick={() => sortTasks('importance')}>Importance</button>
+        <button className="nav-btn" onClick={() => sortTasks('deadline')}>Deadline</button>
+        <button className="nav-btn" onClick={() => sortTasks('created')}>Created</button>
+        <button className="nav-btn" onClick={() => sortTasks('status')}>Status</button>
+        <button className="nav-btn" onClick={() => sortTasks('topic')}>Topic</button>
+        <button className="nav-btn" onClick={() => sortTasks('project')}>Project</button>
+        <button className="nav-btn" onClick={resetSorting}>Reset </button>
+      </section>
     </>
   );
 };
